dec4f9d457539802865f03eddde04dedb30e1901
krishanapoorv1 <86196226+krishanapoorv1@users.noreply.github.com>
2023-05-13 14:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def crop_prediction():
         return render_template('crop-result.html', prediction=final_prediction, pred='img/crop/'+final_prediction+'.jpg')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000, debug=True)
